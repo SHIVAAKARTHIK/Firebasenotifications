6eb898593ef68b0e0795d0ea479634afa3711a48
Karthik M <karthik.manju@actcorp.local>
2018-03-08 11:34:30 +0530

    Initial commit

---
+++ app/src/main/java/info/androidhive/firebasenotifications/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package info.androidhive.firebasenotifications.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import info.androidhive.firebasenotifications.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/info/androidhive/firebasenotifications/app/Config.java
@@ -0,0 +1,23 @@
+package info.androidhive.firebasenotifications.app;
+
+/**
+ * Created by karthik.manju on 07/03/2018.
+ */
+
+//contains all the constant values
+
+public class Config {
+
+    // global topic to receive app wide push notifications
+    public static final String TOPIC_GLOBAL = "global";
+
+    // broadcast receiver intent filters
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+    public static final String PUSH_NOTIFICATION = "pushNotification";
+
+    // id to handle the notification in the notification tray
+    public static final int NOTIFICATION_ID = 100;
+    public static final int NOTIFICATION_ID_BIG_IMAGE = 101;
+
+    public static final String SHARED_PREF = "ah_firebase";
+}
